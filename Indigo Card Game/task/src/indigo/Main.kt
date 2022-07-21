5bdee6d063c7d968f0ad7eae18c236040ed3cc48
Dagim Tesfaye Mebrate <102017186+DagNo1@users.noreply.github.com>
2022-07-21 18:33:10 +0300

    Update Main.kt
    Stage 4 complete, I am getting better at using classes but as the program gets denser the readability lessens.

--- Indigo Card Game/task/src/indigo/Main.kt
+++ Indigo Card Game/task/src/indigo/Main.kt
@@ -2,12 +2,13 @@ package indigo
 
 import kotlin.system.exitProcess
 
-val ALLCARDS = listOf(
+val ALL_CARDS = listOf(
     "A♠","2♠","3♠","4♠","5♠","6♠","7♠","8♠","9♠","10♠","J♠","Q♠","K♠",
     "A♥","2♥","3♥","4♥","5♥","6♥","7♥","8♥","9♥","10♥","J♥","Q♥","K♥",
     "A♦","2♦","3♦","4♦","5♦","6♦","7♦","8♦","9♦","10♦","J♦","Q♦","K♦",
     "A♣","2♣","3♣","4♣","5♣","6♣","7♣","8♣","9♣","10♣","J♣","Q♣","K♣")
-open class Dec(var cards: MutableList<String>){
+val SPECIAL_CARDS = listOf(Regex("A."), Regex("10."),Regex("J."), Regex("Q."), Regex("K."))
+open class DecType(var cards: MutableList<String>, var score: Int = 0, var noOfWonCards: Int = 0){
     init {
         cards.shuffle()
     }
@@ -20,27 +21,36 @@ open class Dec(var cards: MutableList<String>){
         return cardsGotten
     }
     fun add(newCards: MutableList<String>) = cards.addAll(newCards)
-    fun count() = cards.lastIndex + 1
+    fun count() = cards.size
     fun drop(pickedCard: Int): String {
         val card = cards[pickedCard]
         cards.removeAt(pickedCard)
         return card
     }
     fun checkRange(number: Int,upperLimit: Int): Boolean = number in 1..upperLimit
-}
-class Table(cards: MutableList<String>): Dec(cards){
-    fun onTop() = cards[cards.lastIndex]
-    fun addCard(card: String) {
-        cards.add(card)
+    fun addScore(cardsOnTable: MutableList<String>){
+        for (i in 0 until cardsOnTable.size)
+            for (j in 0.. 4)
+                if (SPECIAL_CARDS[j].matches(cardsOnTable[i])) this.score++
+        this.noOfWonCards += cardsOnTable.size
     }
 }
-class Computer(cards: MutableList<String>): Dec(cards){
+class Table(cards: MutableList<String>): DecType(cards){
+    fun onTop() = cards[cards.lastIndex]
+    fun addCard(card: String) = cards.add(card)
+    fun topSymbol() = cards[cards.lastIndex].last()
+    fun showCards() {
+        if (this.count() != 0) println("${this.count()} cards on the table, and the top card is ${this.onTop()}")
+        else println("No cards on the table")
+    }
+}
+class Computer(cards: MutableList<String>): DecType(cards){
     fun tell(): Int {
         println("Computer plays ${cards[0]}")
         return 0
     }
 }
-class Player(cards: MutableList<String>, var turn: Int = 0): Dec(cards){
+class Player(cards: MutableList<String>, var turn: Int = 0): DecType(cards){
     fun pick(): Int{
         print("Cards in hand: ")
         for (i in 0 until this.count()) print("${i + 1})${cards[i]} ")
@@ -68,26 +78,71 @@ fun main() {
         binaryAnswer = readln().lowercase()
         if (binaryAnswer == "no" || binaryAnswer == "yes") break
     }while(true)
-    val dec = Dec(ALLCARDS.toMutableList())
-    val table = Table(dec.get(4))
-    val player = Player(dec.get(6))
-    val computer = Computer(dec.get(6))
+    val decType = DecType(ALL_CARDS.toMutableList())
+    val table = Table(decType.get(4))
+    val player = Player(decType.get(6))
+    val computer = Computer(decType.get(6))
     println("Initial cards on the table: ${table.cards.joinToString(separator = " ")}\n")
+    if (binaryAnswer == "no") player.turn = 1
+    var i = 0
+    var whoWonLast = -1
     do {
-        if (binaryAnswer == "no") player.turn = 1
-        for (i in 0..11) {
-            println("${table.count()} cards on the table, and the top card is ${table.onTop()}")
-            if (i % 2 == player.turn) table.addCard(player.drop(player.pick()))
-            else table.addCard(computer.drop(computer.tell()))
-            if (table.count() == 52) {
-                println("${table.count()} cards on the table, and the top card is ${table.onTop()}")
-                println("Game Over")
-                return
+        table.showCards()
+        var matches = false
+        if (i % 2 == player.turn) {
+            val droppingCard = player.drop(player.pick()) // picking card
+            if(table.count() != 0) matches = checkIfSame(table, droppingCard)
+            table.addCard(droppingCard)
+            if (matches) {
+                player.addScore(table.get(table.count()))
+                println("Player wins cards")
+                whoWonLast = 0
+                showScore(player, computer)
+                println()
             }
-            if (i == 11) {
-                player.add(dec.get(6))
-                computer.add(dec.get(6))
+        } else {
+            val droppingCard = computer.drop(computer.tell())
+            if(table.count() != 0) matches = checkIfSame(table, droppingCard)
+            table.addCard(droppingCard)
+            if (matches) {
+                computer.addScore(table.get(table.count()))
+                println("Computer wins cards")
+                whoWonLast = 1
+                showScore(player, computer)
+                println()
             }
         }
+        gameOver(table, player, computer, whoWonLast)
+        if (i == 11) {
+            player.add(decType.get(6))
+            computer.add(decType.get(6))
+            i = -1
+        }
+        i++
     } while (true)
+}
+fun checkIfSame(table: Table, droppedCard: String): Boolean {
+    val suit  = "..?${table.topSymbol()}".toRegex()
+    val rank = "${table.onTop()[0]}.?.?".toRegex()
+    return suit.matches(droppedCard) || rank.matches(droppedCard)
+}
+fun showScore(player: Player, computer: Computer) {
+    println("Score: Player ${player.score} - Computer ${computer.score}")
+    println("Cards: Player ${player.noOfWonCards} - Computer ${computer.noOfWonCards}\n")
+}
+fun gameOver(table: Table, player: Player, computer: Computer, wonLast: Int){
+    if (table.count() + player.noOfWonCards + computer.noOfWonCards != 52) return
+    println()
+    table.showCards()
+    if (wonLast == 0 && table.count() != 0)  player.addScore(table.get(table.count()))
+    else if (wonLast == 1 && table.count() != 0) computer.addScore(table.get(table.count()))
+    else if (wonLast == -1) if (player.turn == 0) player.addScore(table.get(table.count())) else computer.addScore(table.get(table.count()))
+    when {
+        player.noOfWonCards > computer.noOfWonCards -> player.score += 3
+        player.noOfWonCards < computer.noOfWonCards -> computer.score += 3
+        player.noOfWonCards == computer.noOfWonCards -> if (player.turn == 0) player.score += 3 else computer.score += 3
+    }
+    showScore(player, computer)
+    println("Game Over")
+    exitProcess(0)
 }
